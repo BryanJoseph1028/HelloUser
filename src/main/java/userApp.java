e5d618110fe9d08e0ab3a61edba8910b0ba34c26
bryan <bryancorado777.com>
2022-12-12 20:15:39 -0600

    first commit

---
+++ src/main/java/userApp.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author bryan1010p
+ */
+public class userApp {
+ public static void main(String[] args){
+     System.out.print("ingrese su nombre");
+     Scanner scanner = new Scanner(System.in);
+     
+ }   
+}
